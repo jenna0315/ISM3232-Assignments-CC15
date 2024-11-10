fe59a4a4c7737c99895a6d2b6b924b0403ee01b0
Your Name <you@example.com>
2024-11-10 18:26:34 -0500

    Task 2: Create portfolio module with calculatePortfolioValue and getPortfolioAllocation functions.

--- asset.js
+++ asset.js
@@ -1,6 +1,6 @@
 //Task 1: Create asset module with asset list and getAssetById function.
 //creation of asset module
-export const assets = [
+export const assetList = [
     { id: 1, name: 'Apple Inc.', type: 'stock', price: 1200, quantity: 40 },
     { id: 2, name: 'US Treasury Bond', type: 'bond', price: 150.00, quantity:100},
     { id: 3, name: 'Amazon Inc.', type: 'stock', price: 200.00, quantity:10}
@@ -8,5 +8,5 @@ export const assets = [
 
 //create getAssetById function
 export function getAssetById(id) {
-    return assets.find(asset => asset.id === id);
+    return assetList.find(asset => asset.id === id);
 }
---
+++ portfolio.js
@@ -0,0 +1,31 @@
+//Task 2: Create portfolio module with calculatePortfolioValue and getPortfolioAllocation functions.
+//Import assets
+import { assetList, getAssetById } from './asset.js';
+
+console.log('Asset List:', assetList);
+console.log('Asset with ID 1:', getAssetById(1));
+
+//Define and export a function calculatePortfolioValue()
+export function calculatePortfolioValue() {
+    return assetList.reduce((total, asset) => {
+        return total + (asset.price * asset.quantity);
+    }, 0);
+};
+
+//Export a function getPortfolioAllocation()
+export function getPortfolioAllocation() {
+    const totalPortfolioValue = calculatePortfolioValue();
+    
+    const allocation = assets.map(asset => {
+      const assetValue = asset.price * asset.quantity;
+      const percentage = (assetValue/totalPortfolioValue) * 100;
+      
+      return {
+        id: asset.id,
+        name: asset.name,
+        allocationPercentage: percentage.toFixed(2)
+      };
+    });
+  
+    return allocation;
+  }
