ec4d6119a729f1d108a73786b744c4cfbb545aff
Your Name <you@example.com>
2024-11-10 17:35:36 -0500

    Task 1: Create asset module with asset list and getAssetById function.

---
+++ asset.js
@@ -0,0 +1,12 @@
+//Task 1: Create asset module with asset list and getAssetById function.
+//creation of asset module
+export const assets = [
+    { id: 1, name: 'Apple Inc.', type: 'stock', price: 1200, quantity: 40 },
+    { id: 2, name: 'US Treasury Bond', type: 'bond', price: 150.00, quantity:100},
+    { id: 3, name: 'Amazon Inc.', type: 'stock', price: 200.00, quantity:10}
+];
+
+//create getAssetById function
+export function getAssetById(id) {
+    return assets.find(asset => asset.id === id);
+}
